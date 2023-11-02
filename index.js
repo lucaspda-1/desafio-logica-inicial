a31720baaa09bb96ea3c3738c255fc53d0fe7867
lucaspda-1 <lpa4@aluno.ifal.edu.br>
2023-11-02 12:35:52 -0300

    desafio DIO

---
+++ index.js
@@ -0,0 +1,21 @@
+let nome = 'Zé';
+let xp = 10001;
+
+if(xp <=1000){
+    console.log('O Herói de nome '+nome+ ' está no nível de Ferro')
+    
+}else if(xp <= 2000){
+    console.log('O Herói de nome '+nome+ ' está no nível de Bronze')
+}else if(xp <= 5000){
+    console.log('O Herói de nome '+nome+ ' está no nível de Prata')
+}else if(xp <= 7000){
+    console.log('O Herói de nome '+nome+ ' está no nível de Ouro')
+}else if(xp <= 8000){
+    console.log('O Herói de nome '+nome+ ' está no nível de Platina')
+}else if(xp <= 9000){
+    console.log('O Herói de nome '+nome+ ' está no nível de Ascendente')
+}else if(xp <= 10000){
+    console.log('O Herói de nome '+nome+ ' está no nível de Imortal')
+}else if(xp > 10000){
+    console.log('O Herói de nome '+nome+ ' está no nível de Radiante')
+}
